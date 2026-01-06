991a9727f5d58481c10caeeb719e86dc16de5075
wenchiet <280592199@qq.com>
2026-01-06 10:12:44 +0800

    清理文件

--- test_lunar.ts
+++
@@ -1,8 +0,0 @@
-import { Lunar } from 'lunar-javascript';
-
-try {
-    const d = Lunar.fromDate(new Date());
-    console.log("Lunar Test Success:", d.toFullString());
-} catch (e) {
-    console.error("Lunar Test Failed:", e);
-}
